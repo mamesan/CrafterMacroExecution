06639def71f2e05d5ba90c2a9381e9a6fce7e7ba
endo <p-endo@presc.jp>
2019-08-16 13:48:28 +0900

    プロジェクト ファイルを追加します。

---
+++ CrafterMacroExecution/CrafterMacroExecution.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CrafterMacroExecution
+{
+    public partial class CrafterMacroExecution : UserControl
+    {
+        public CrafterMacroExecution()
+        {
+            InitializeComponent();
+        }
+    }
+}
